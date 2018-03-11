bae1847abb0f0329c2b890ecee2feb1eff3bc9b5
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-03-11 14:01:30 -0700

    updated data file

--- data.js
+++ data.js
@@ -4,27 +4,27 @@
           [
               {"name": "rice", "qty": 2, "units": "cups of"},
               {"name": "chicken or vegetable stock", "qty": 1, "units": "quart"},
-              {"name": "shrimp", "qty": 0.5, "units": "pound" },
-              {"name": "red, green, or orange pepper", "qty": 1, "units": "medium"},
+              {"name": "shrimp", "qty": 1, "units": "pound" },
+              {"name": "tomato", "qty": 2, "units": "medium" },
               {"name": "saffron", "qty": 15, "units": "threads"},
               {"name": "garlic", "qty": 4, "units": "cloves of"},
-              {"name": "salt", "qty": 1, "units": "tablespoon"},
+              {"name": "salt", "qty": 2, "units": "teaspoons"},
               {"name": "black pepper or red pepper flakes", "qty": 1, "units": "teaspoon"},
               {"name": "olive oil", "qty": 4, "units": "tablespoons"},
           ],
       "steps" :
       [
-          "First step paella recipe.",
-          "Second step paella recipe..",
-          "Third step paella recipe.",
-          "Serve."
+          "The ingredients you will need to collect are 2 cups of paella rice or your preferred rice, 1 quart of vegetable or chicken stock, 1 pound of shrimp, about 15 threads of saffron, 4 cloves of garlic, 2 teaspoons of salt, 1 teaspoon of black pepper or red pepper falkes, and about 4 tablespoons of olive oil. The materials you will need are a 10 to 12 inch skillet, 1 saucepan, 1 chef\'s knife, and bowl or collander to rinse the rice.",
+          "Rinse the rice in a collander or bowl until the water runs clear, then drain off the excess water. Remove the skin from the onion and garlic cloves, then quarter and dice the onion, then press the garlic in a garlic press, or alternately, chop it up finely. Wash the tomatoes and then quarter them and dice them into small chunks. Add the olive oil to the skillet and heat to medium heat. Add the onions and stir occassionally for about 4 minutes or until they become translucent. Then add in the garlic, tomatoes, salt, pepper, and saffron. Let this mixture simmer on medium heat until it reduces in volume, about another 3 to 5 minutes, stirring occassionally. Add the rice and stir to coat.",
+          "Add the chicken or vegetable stock to the saucepan and bring to a boil. Add in the rice mixture and stir once to combine. Reduce the heat to medium-low and set a timer for 10 minutes while you prepare the shrimp. Deshell and devein the shrimp. Once the 10 minutes is up, add the shrimp to the rice mixture and cook for another 8 to 10 minutes. Turn the pan occassionally to ensure the mixture cooks evenly.",
+          "Let cool for a few minutes, and serve."
       ],
       "details" :
       [
-        "Here is your first detailed tip for paella.",
-        "Here is the second detailed tip for paella",
-        "Here is the third detail for paella",
-        "Here is the fourth detail for paella",
+        "Any type of rice is fine to use, we recommend paella rice or brown rice. If using wild rice, it can take up to 40 minutes to cook so you may need to add additional broth and adjust the cooking time accordingly.",
+        "For a vegetarian option, use vegetable broth and substitute vegan or vegetarian chorizo, cooked following package instructions, which you would add at the end when the rice is finished cooking.",
+        "Mussels or clams can be used in addition to, or as a substitution for, the shrimp.",
+        "As an optional garnish, top with chopped fresh parsley, or oregano.",
       ]
   },
       "tortilla": {
@@ -60,8 +60,8 @@
        "gazpacho": {
       "ingredients" :
           [
-              {"name": "tomatoes", "qty": 1, "units": "quart"},
-              {"name": "cucumbers", "qty": 1, "units": "large"},
+              {"name": "tomatoes", "qty": 4, "units": "large tomatoes, or 20 to 30 cherry tomatoes"},
+              {"name": "cucumbers", "qty": 1, "units": "large or 2 to 3 smaller cucumbers"},
               {"name": "cilantro", "qty": 0.5, "units": "bunch"},
               {"name": "onion", "qty": 0.5, "units": "medium"  },
               {"name": "olive oil", "qty": 2, "units": "tablespoons" },
@@ -72,16 +72,16 @@
           ],
       "steps" :
       [
-          "The ingredients you will need to collect are one quart of tomatoes, 1 onion, 1 lime, one-half a teaspoon each of salt and pepper, 1 large cucumber or 2-3 smaller persian cucumbers, 2 tablespoons of olive oil, a teaspoon of basalmic vinegar, and optionally about one-half of a bunch of fresh cilantro. The materials you will need are one large mixing bowl, 1 chef's knife, and a spoon for mixing. Wash the tomatoes, cucumber, and cilantro, and remove the skin from the onion. ",
-          "Quarter the vegetables and then dice them into evenly sized chunks. If you are using cilantro, cut off the bottom stem portion, roll the leafy portion into a bundle, and finely dice it. Slice the lime through the center.",
+          "The ingredients you will need to collect are one quart of tomatoes, 1 onion, 1 lime, one-half a teaspoon each of salt and pepper, 1 large cucumber or 2 to 3 smaller persian cucumbers, 2 tablespoons of olive oil, a teaspoon of basalmic vinegar, and optionally about one-half of a bunch of fresh cilantro. The materials you will need are one large mixing bowl, 1 chef's knife, a vegetable peeler, and a spoon for mixing. Wash the tomatoes, cucumber, and cilantro, and remove the skin from the onion. ",
+          "Use a peeler to skin the cucumber, then dice it into evenly sized chunks. Quarter the tomatoes and onion and then dice them into evenly sized chunks. If you are using cilantro, cut off the majority of the stem portion and discard it. Then roll the leafy portion into a bundle, and finely chop it. Roll the lime on a hard surface under your palm to soften, then slice the lime through the center.",
           " If you are using a food processor, add the vegetables, olive oil, balsamic, salt, and pepper, then squeeze the lime on top of the mixture. Cover the container and pulse until the mixture reaches the desired consistency. Alternatively, combine all of the ingredients in a mixing bowl and stir to combine. Cover the mixing bowl or food processing container and transfer it to the refrigerator to cool.",
           "Serve."
       ],
        "details" :
       [
-        "For a zestier taste, add more raw onion, or about a half of a clove of diced raw garlic. For a milder taste, reduce the amount of onion.",
-        "Rolling the lime on a hard surface using your palm will make it easier to juice. Don\'t have a lime? You can substitute lemon or lemon concentrate.",
-        "Gazpacho is great on its own as a side dish, or served with a crusty baguette, or crackers."
+        "For a zestier taste, add about a half of a clove of diced raw garlic, a pinch of cayenne pepper, or about a half teaspoon of red pepper flakes. For a milder taste, reduce the amount of fresh onion.",
+        "Don\'t have a lime? You can substitute lemon or lemon concentrate. Meyer\'s lemons have a sweet and tangy flavor, which makes a nice substition.",
+        "Gazpacho is great on its own as a side dish, or served with a crusty baguette."
       ]
     },
      "tapas": {
@@ -90,22 +90,23 @@
               {"name": "olive oil",  "qty": 1, "units": "tablespoon"},
               {"name": "honey or agave",  "qty": 1, "units": "tablespoon"},
               {"name": "baguette",  "qty": 1, "units": "fresh"},
-              {"name": "brie or chev cheese", "qty": 10, "units": "ounces"  }
+              {"name": "brie or chev cheese", "qty": 4, "units": "ounces"},
+              {"name": "sea salt", "qty": 1, "units": "teaspoon"},
+              {"name": "black pepper", "qty": 1, "units": "teaspoon"},
+              {"name": "walnuts", "qty": 0.5, "units": "cup"  }
           ],
       "steps" :
       [
-          "First step for tapas recipe.",
-          "Second step for tapas recipe.",
-          "Third step for tapas recipe",
-          "Fourth step for tapas recipe.",
-          "Serve."
+          "The ingredients you will need to collect are one baguette, about a Tablespoon of honey or agave, a teaspoon each of salt and pepper, and optionally, about a half-cup of walnuts for garnish. The materials you will need are a serrated knife or bread knife, a chef's knife, and an oven safe pan.",
+          "Preheat the oven to 375 degrees Farenheit. Let the cheese warm to room temperature. Slice the baguette into even slices, about an inch thick. Drizzle each slice with olive oil. Spread the cheese evenly over each slice. Depending on your preference, you can dice the walnuts or leave them as is. Add the walnuts on top of the cheese. Drizzle honey or agave across the top of each piece. Sprinkle a small amount of salt and pepper over each slice.",
+          "Place the bread on a pan and into the oven on the top rack. Bake for about 5 minutes or until the walnuts begin to soften.",
+          "Remove from the oven and let cool to room temperature before serving."
       ],
        "details" :
       [
-        "Here is your first detailed tip for tapas.",
-        "Here is the second detailed tip for tapas",
-        "Here is the third detail for tapas",
-        "Here is the fourth detail for tapas",
+        "Want to reduce the amount of cheese used? Wash and slice a large tomato into large, even slices. Add the tomato under the cheese or alterante slices of bread between cheese and tomato.",
+        "Don\'t have walnuts? No problem. You can substitute almonds.",
+        "You can substitute mangecho, iberico, or even a sharp cheddar cheese for this recipe, depending on your preference. For a vegan option, opt for tomatoes or vegan cheese and substitute agave for honey."
       ]
      },
   };
